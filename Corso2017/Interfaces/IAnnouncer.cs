3de3b1b0d7eedda5c9148a5b70d8854f62498524
euris-maran <daniel.maran@euris.it>
2017-10-19 10:50:23 +0200

    Interfaces Observer

---
+++ Corso2017/Interfaces/IAnnouncer.cs
@@ -0,0 +1,21 @@
+﻿using Interfaces.Writers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IAnnouncer
+    {
+        void AddWriter(IWriter writer);
+
+        void Write();
+
+        //void Write(IWriter writer);
+
+        void Write(TextWriter writer);
+    }
+}
