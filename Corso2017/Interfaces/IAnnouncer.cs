c69841f6f7c80ca99d0a425daac9d8d534c6ad66
euris-maran <daniel.maran@euris.it>
2017-10-19 11:01:03 +0200

    Pseudocodice unit test

--- Corso2017/Interfaces/IAnnouncer.cs
+++ Corso2017/Interfaces/IAnnouncer.cs
@@ -1,6 +1,7 @@
 ﻿using Interfaces.Writers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,29 @@ using System.Threading.Tasks;
 
 namespace Interfaces
 {
+    //enum LanguageEnum { Ita, Eng }
+ 
+    //class Test
+    //{
+    //    void TestHelloIta()
+    //    {
+    //        IAnnouncer ann = null;
+    //        string hello = ann.Hello(LanguageEnum.Ita);
+    //        Debug.Assert(hello == "ciao");
+    //    }
+
+    //    void TestHelloEng()
+    //    {
+    //        IAnnouncer ann = null;
+    //        string hello = ann.Hello(LanguageEnum.Eng);
+    //        Debug.Assert(hello == "hello");
+    //    }
+    //}
+
     interface IAnnouncer
     {
+        //string Hello(LanguageEnum lang);
+
         void AddWriter(IWriter writer);
 
         void Write();
