64612fdec247bfd558dc49420dc6e544dded7280
euris-maran <daniel.maran@euris.it>
2017-10-23 14:09:25 +0200

    Skeleton logger

---
+++ Corso2017/SuperLoggerTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLoggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureLog();
+
+            TestLogInfo();
+            TestLogError();
+
+            Console.ReadKey(true);
+        }
+
+        private static void ConfigureLog()
+        {
+            //configure logger for any writing method
+        }
+
+        private static void TestLogInfo()
+        {
+            //do something
+            //logger.LogInfo("") or
+            //logger.Log(Info, "") or
+            //Logger.Log(...)
+        }
+
+        private static void TestLogError()
+        {
+            //do something
+            //try/catch
+            //logger.LogError("", ex) or
+            //logger.Log(Error, "", ex) or
+            //Logger.Log(...)
+            throw new Exception("This is a test exception");
+        }
+    }
+}
