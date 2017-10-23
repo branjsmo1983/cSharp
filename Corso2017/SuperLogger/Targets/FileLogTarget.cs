29d4239a294999ef49aa3609db7b25c86ea9d57d
euris-maran <daniel.maran@euris.it>
2017-10-23 16:45:27 +0200

    Skeleton FileWriter

---
+++ Corso2017/SuperLogger/Targets/FileLogTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLogger.Targets
+{
+    public class FileLogTarget : ILogTarget
+    {
+        public void WriteLog(LogEntry info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
