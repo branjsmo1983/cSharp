4463bb348ebf4091229e25c08ce6dbc007f8e723
euris-maran <daniel.maran@euris.it>
2017-11-10 13:00:15 +0100

    Bozza db studenti

---
+++ Corso2017/Corsi/Tables/Corso.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Corso]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(50) NOT NULL, 
+    [Id_Studente] INT NULL, 
+    CONSTRAINT [FK_Corso_Studente] FOREIGN KEY ([Id_Studente]) REFERENCES [Studente]([Id])
+)
---
+++ Corso2017/Corsi/Tables/Studente.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Studente] (
+    [Id]   INT           NOT NULL,
+    [Nome] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
