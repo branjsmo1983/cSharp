d871c285fecfd79d36c8e08dbbe02655adb0aff3
euris-maran <daniel.maran@euris.it>
2017-11-14 11:55:26 +0100

    View varie

---
+++ Corso2017/Corsi/dbo/Views/NumeroAlunniPerCorso.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE view [dbo].[NumeroAlunniPerCorso] as
+select 
+	--c.Id,
+	c.Nome Corso,
+	doc.Nome + ' ' + doc.Cognome Docente,
+	count(isc.Id_Corso) as N_Alunni
+ from
+	Corso as c
+	inner join 
+	Docente as Doc on 
+		Doc.CodiceFiscale = c.CF_Docente
+	left join 
+	Iscrizione as isc on 
+		isc.Id_Corso = c.Id
+ group by 
+	c.Id,
+	c.Nome, 
+	doc.Nome + ' ' + doc.Cognome
