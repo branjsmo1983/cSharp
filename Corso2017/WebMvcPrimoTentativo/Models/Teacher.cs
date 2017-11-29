e83a4900502c10f13b35495265ff1406e953cc80
euris-maran <daniel.maran@euris.it>
2017-11-29 13:07:37 +0100

    Validazioni, jquery validations, attributi sui models; progetto Superheroes creato.

--- Corso2017/WebMvcPrimoTentativo/Models/Teacher.cs
+++ Corso2017/WebMvcPrimoTentativo/Models/Teacher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,14 @@ namespace WebMvcPrimoTentativo.Models
     public class Teacher
     {
         public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w ])+$")]
+        [MinLength(3)]
+        [MaxLength(32)]
         public string Name { get; set; }
+
+        [Range(1, 5)]
         public int Rating { get; set; }
     }
 }
