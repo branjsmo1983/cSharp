a2b17049326419ec1b6767946e01940f1f6c9a46
euris-maran <daniel.maran@euris.it>
2017-11-30 10:37:52 +0100

    complex Linq plays (part 1)

---
+++ Corso2017/LinqPlays/ComplexAgents.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPlays
+{
+    static class ComplexAgents
+    {
+        public static void Play()
+        {
+            var agents = ComplexAgent.Create();
+
+            Console.WriteLine("Total income for every agent:");
+
+            var agentsWithTotal = agents
+                .Select(a => new
+                {
+                    Name = a.Name,
+                    TotalIncome = a.Operations.Sum(o => o.Income)
+                });
+
+            foreach(var i in agentsWithTotal)
+                Console.WriteLine($"L'agente {i.Name} ha totalizzato {i.TotalIncome} €");
+        }
+    }
+
+    class ComplexAgent
+    {
+        public static List<ComplexAgent> Create()
+        {
+            return new List<ComplexAgent>
+            {
+                Create("Bob Martin", "UD", new List<Operation>
+                {
+                    Operation.Create(2017, 11, 29, 5000),
+                    Operation.Create(2017, 11, 30, 7000),
+                }),
+                Create("Phil Collins", "TS", new List<Operation>
+                {
+                    Operation.Create(2017, 11, 28, 1000),
+                    Operation.Create(2017, 11, 29, 2000),
+                }),
+                Create("Matthew Core", "UD", new List<Operation>()),
+                Create("Mark Zuck", "PD", new List<Operation>
+                {
+                    Operation.Create(2017, 11, 28, 7000),
+                    Operation.Create(2017, 11, 29, 9000),
+                    Operation.Create(2017, 11, 30, 3000),
+                }),
+                Create("Eric Evans", "TS", new List<Operation>
+                {
+                    Operation.Create(2017, 11, 27, 2000),
+                    Operation.Create(2017, 11, 28, 2000),
+                    Operation.Create(2017, 11, 29, 2000),
+                    Operation.Create(2017, 11, 30, 2000),
+                })
+            };
+        }
+
+        public static ComplexAgent Create(
+            string name, string district, List<Operation> operations)
+        {
+            return new ComplexAgent
+            {
+                Name = name,
+                District = district,
+                Operations = operations
+            };
+        }
+
+        public string Name { get; set; }
+        public string District { get; set; }
+        public List<Operation> Operations { get; set; }
+    }
+
+    class Operation
+    {
+        public static Operation Create(int year, int month, int day, int income)
+        {
+            return new Operation
+            {
+                Date = new DateTime(year, month, day),
+                Income = income,
+            };
+        }
+
+        public DateTime Date { get; set; }
+        public int Income { get; set; }
+    }
+
+}
---
+++ Corso2017/LinqPlays/SimpleAgents.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPlays
+{
+    static class SimpleAgents
+    {
+        public static void Play()
+        {
+            var agents = SimpleAgent.Create();
+
+            Console.WriteLine("Select dopo aver creato una classe DistrictIncome per lo scopo");
+
+            var districtIncomes = agents
+                .GroupBy(x => x.District)
+                .Select(x => new DistrictIncome
+                    {
+                        District = x.Key,
+                        TotalIncome = x.Sum(a => a.TotalIncome)
+                    });
+
+            foreach(var di in districtIncomes)
+                Console.WriteLine($"La provincia {di.District} ha raggiunto {di.TotalIncome} di guadagno");
+
+            Console.WriteLine();
+
+            var anonymousDistrictIncomes = agents
+                .GroupBy(x => x.District)
+                .Select(x => new
+                    {
+                        District = x.Key,
+                        TotalIncome = x.Sum(a => a.TotalIncome)
+                    });
+
+            foreach (var di in anonymousDistrictIncomes)
+                Console.WriteLine($"La provincia {di.District.ToUpper()} ha raggiunto {di.TotalIncome} di guadagno");
+        }
+    }
+
+    class SimpleAgent
+    {
+        public static List<SimpleAgent> Create()
+        {
+            return new List<SimpleAgent>
+            {
+                Create("Bob Martin", "UD", 30000),
+                Create("Phil Collins", "TS", 25000),
+                Create("Matthew Core", "UD", 45000),
+                Create("Mark Zuck", "PD", 47000),
+                Create("Eric Evans", "TS", 18000)
+            };
+        }
+
+        public static SimpleAgent Create(string name, string district, int totalIncome)
+        {
+            return new SimpleAgent
+            {
+                Name = name,
+                District = district,
+                TotalIncome = totalIncome
+            };
+        }
+
+        public string Name { get; set; }
+        public string District { get; set; }
+        public int TotalIncome { get; set; }
+    }
+
+    class DistrictIncome
+    {
+        public string District { get; set; }
+        public int TotalIncome { get; set; }
+    }
+}
