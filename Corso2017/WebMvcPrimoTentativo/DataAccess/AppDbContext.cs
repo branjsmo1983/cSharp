d7a2a7e38788280ae9f91fae618f963e345a1c3f
euris-maran <daniel.maran@euris.it>
2017-11-30 11:06:34 +0100

    Entity Framework (incomplete)

--- Corso2017/WebMvcPrimoTentativo/DataAccess/AppDbContext.cs
+++ Corso2017/WebMvcPrimoTentativo/DataAccess/AppDbContext.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebMvcPrimoTentativo.Models;
 
 namespace WebMvcPrimoTentativo.DataAccess
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext()
+            : base()
+        { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
         public DbSet<Teacher> Teachers { get; set; }
     }
 }
