eda40835bcddbad0e4e753fd70d746e5c15806cd
Adamo Branz <branjsmo@hotmail.com>
2017-11-30 17:48:53 +0100

    prova

--- Corso2017/WebMvcSuperheroes/Models/SuperHero.cs
+++ Corso2017/WebMvcSuperheroes/Models/SuperHero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace WebMvcSuperheroes.Models
 
         [Required]
         [MaxLength(50)]
+        [DisplayName("Hero Name")]
         public string HeroName { get; set; }
 
         [MaxLength(50)]
