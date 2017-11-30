65091c9872be7add08aec8625a0d0e7d2d48a825
euris-maran <daniel.maran@euris.it>
2017-11-30 17:37:47 +0100

    SuperHeroes (part 3)

--- Corso2017/WebMvcSuperheroes/Models/Villain.cs
+++ Corso2017/WebMvcSuperheroes/Models/Villain.cs
@@ -1,9 +1,18 @@
-﻿namespace WebMvcSuperheroes.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMvcSuperheroes.Models
 {
     public class Villain
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public int NemesisId { get; set; }
+
+        //l'attributo serve se proprietà con l'oggetto e proprietà con l'Id
+        //non coincidono
+        //[ForeignKey(nameof(NemesisId))]
+        public SuperHero Nemesis { get; set; }
     }
 }
